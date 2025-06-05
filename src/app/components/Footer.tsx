1e7004f8aa1fa92ef38b7236007b33da3235943a
Илья Семёнов <ilya.semyonov.03@mail.ru>
2025-06-06 02:44:59 +0300

    feat: footer links hover scale

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -7,7 +7,11 @@ const Footer = () => {
     <footer className="flex items-center">
       <div className="bg-accent flex items-center justify-center rounded-tl-[50px] rounded-tr-[50px] py-5 gap-12 min-w-[679px] w-full">
         {footerLinks.map((e, i) => (
-          <Link key={i} href={e.href}>
+          <Link
+            className="hover:scale-115 transition-transform duration-300"
+            key={i}
+            href={e.href}
+          >
             {e.icon as React.ReactNode}
           </Link>
         ))}
