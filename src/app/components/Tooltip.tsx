55bff4a85ffc768940eba60124386978783c380b
Илья Семёнов <ilya.semyonov.03@mail.ru>
2025-06-06 01:41:18 +0300

    feat: tooltip animation

--- src/app/components/Tooltip.tsx
+++ src/app/components/Tooltip.tsx
@@ -1,4 +1,7 @@
-import React, { useState } from "react";
+"use client";
+
+import type React from "react";
+import { useState } from "react";
 
 interface TooltipProps {
   text: string;
@@ -17,14 +20,18 @@ export const Tooltip = ({ children, text }: TooltipProps) => {
       >
         {children}
       </div>
-      {isVisible && (
-        <div className="absolute z-10 px-3 py-2 text-sm text-white bg-gray-900 rounded-lg shadow-lg bottom-full left-1/2 transform -translate-x-1/2 mb-2 whitespace-nowrap">
-          {text}
-          <div className="absolute top-full left-1/2 transform -translate-x-1/2">
-            <div className="border-4 border-transparent border-t-gray-900"></div>
-          </div>
+      <div
+        className={`border-1 border-accent absolute z-10 px-3 py-2 text-sm text-light-gray bg-main-white rounded-lg shadow-lg bottom-full left-1/2 transform -translate-x-1/2 mb-2 whitespace-nowrap transition-all duration-200 ease-in-out ${
+          isVisible
+            ? "opacity-100 translate-y-0 pointer-events-auto"
+            : "opacity-0 translate-y-1 pointer-events-none"
+        }`}
+      >
+        {text}
+        <div className="absolute top-full left-1/2 transform -translate-x-1/2">
+          <div className="border-4 border-transparent border-t-accent"></div>
         </div>
-      )}
+      </div>
     </div>
   );
 };
