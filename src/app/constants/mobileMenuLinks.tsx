e90c8f655d3d056958e6ff33d13adfe86a0a38f0
Илья Семёнов <ilya.semyonov.03@mail.ru>
2025-06-06 19:38:35 +0300

    feat:icons &  config for mobile menu

---
+++ src/app/constants/mobileMenuLinks.tsx
@@ -0,0 +1,39 @@
+import ShopWindowIcon from "@/app/icons/ShopWindowIcon";
+import BookIcon from "@/app/icons/BookIcon";
+import PresentIcon from "@/app/icons/PresentIcon";
+import ChestIcon from "@/app/icons/ChestIcon";
+import PortfolioIcon from "@/app/icons/PortfolioIcon";
+import TrophyIcon from "@/app/icons/TrophyIcon";
+
+export const mobileMenuLinks = [
+  {
+    title: "Market",
+    href: "/",
+    icon: <ShopWindowIcon />,
+  },
+  {
+    title: "Leaderboard",
+    href: "/",
+    icon: <TrophyIcon />,
+  },
+  {
+    title: "Portfolio",
+    href: "/",
+    icon: <PortfolioIcon />,
+  },
+  {
+    title: "Stake Jack",
+    href: "/",
+    icon: <ChestIcon />,
+  },
+  {
+    title: "Claim $JACK",
+    href: "/",
+    icon: <PresentIcon />,
+  },
+  {
+    title: "Education",
+    href: "/",
+    icon: <BookIcon />,
+  },
+];
