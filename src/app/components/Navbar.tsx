9e9dd88eaf5cd7b03b38a967ee30d7d3129ba8c6
Илья Семёнов <ilya.semyonov.03@mail.ru>
2025-06-06 03:15:42 +0300

    refactor: tailwind properties

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -13,12 +13,12 @@ interface NavbarProps {
 const Navbar = ({ links }: NavbarProps) => {
   const pathName = usePathname();
   return (
-    <nav className="rounded-full  px-[74px] py-[13px] bg-main-bg shadow-main-shadow">
+    <nav className="rounded-full px-18.5 py-3 bg-main-bg shadow-main-shadow">
       <ul className="flex items-center gap-[30px]">
         {links.map((e) => (
           <li key={e.title}>
             <Link
-              className={`duration-300  block text-xl py-[20px] px-[55px] rounded-full ${
+              className={`duration-300  block text-xl py-5 px-14 rounded-full ${
                 pathName.includes(e.href)
                   ? "bg-accent text-gray-50"
                   : "text-soft-red hover:text-accent"
--- src/app/components/TotalValueLocked.tsx
+++ src/app/components/TotalValueLocked.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React from "react";
+
 interface TotalValueLockedProps {
-    value:string
+  value: string;
 }
-const TotalValueLocked = ({value}:TotalValueLockedProps) => {
-    return (
-        <div className='bg-main-bg rounded-lg py-[12px] px-[34px] shadow-main-shadow w-full flex items-center text-accent justify-between'>
-            <p className='text-3xl font-medium'>Total Value Locked:</p>
-            <p className='text-4xl font-bold'>${value}</p>
-        </div>
-    );
+
+const TotalValueLocked = ({ value }: TotalValueLockedProps) => {
+  return (
+    <div className="bg-main-bg rounded-lg py-3 px-8.5 shadow-main-shadow w-full flex items-center text-accent justify-between">
+      <p className="text-3xl font-medium">Total Value Locked:</p>
+      <p className="text-4xl font-bold">${value}</p>
+    </div>
+  );
 };
 
 export default TotalValueLocked;
